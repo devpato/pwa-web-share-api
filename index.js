78c34ab087a370c378ccd7a7ec2886b801a7d127
Pato <pevargas2730@gmail.com>
2020-01-21 00:00:54 -0500

    pwa share api logic

---
+++ index.js
@@ -0,0 +1,36 @@
+// Index.js
+
+initializeApp();
+
+function onShare() {
+  const title = document.title;
+  const url = document.querySelector("link[rel=canonical]")
+    ? document.querySelector("link[rel=canonical]").href
+    : document.location.href;
+
+  if (navigator.share) {
+    navigator
+      .share({
+        title,
+        url
+      })
+      .then(() => {
+        alert(`Thanks for Sharing!`);
+      })
+      .catch(err => {
+        alert(`Couldn't share ${err}`);
+      });
+  } else {
+    alert(`Not supported 🙅!!`);
+  }
+}
+
+function initializeApp() {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js").then(() => {
+      document.querySelector("#share").addEventListener("click", () => {
+        onShare();
+      });
+    });
+  }
+}
